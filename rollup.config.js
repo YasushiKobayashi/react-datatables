746bdce9feb4c83c7bf6c50c460e748e2839b09d
YasushiKobayashi <ptpadan@gmail.com>
2019-05-05 00:27:48 +0900

    Add type info

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import babili from 'rollup-plugin-babili';
+import { dts } from 'rollup-plugin-dts';
 
 const plugins = [nodeResolve(), commonjs({ jsnext: true }), typescript()];
 
@@ -9,15 +10,23 @@ if (process.env.NODE_ENV === 'production') {
   plugins.push(babili({ comments: false }));
 }
 
-export default {
-  input: './src/index.tsx',
-  output: [
-    {
-      sourcemap: true,
-      file: './dist/index.js',
-      format: 'cjs',
-    },
-  ],
-  external: ['react', 'react-is'],
-  plugins,
-};
+export default [
+  {
+    input: './src/index.tsx',
+    output: [
+      {
+        sourcemap: true,
+        file: './dist/index.js',
+        format: 'cjs',
+      },
+    ],
+    external: ['react', 'react-is'],
+    plugins,
+  },
+  {
+    input: './src/index.tsx',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    external: ['react', 'react-is'],
+    plugins: [dts()],
+  },
+];
